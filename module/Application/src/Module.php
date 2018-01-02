203bdad8a236dd11370e12c14aff7d5876c1ecc6
Bassin Laurent <laurent@bassin.info>
2018-01-02 22:25:02 +0100

    Add php stict_types

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -1,17 +1,25 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
+
+declare(strict_types=1);
 
 namespace Application;
 
+/**
+ * Class Module
+ *
+ * @author Laurent Bassin <laurent@bassin.info>
+ */
 class Module
 {
+    /**
+     *
+     */
     const VERSION = '3.0.3-dev';
 
-    public function getConfig()
+    /**
+     * @return mixed
+     */
+    public function getConfig(): array
     {
         return include __DIR__ . '/../config/module.config.php';
     }
